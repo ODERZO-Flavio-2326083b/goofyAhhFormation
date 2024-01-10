a38cd9d04c5bd0cecfe294e61d36c0eb868aecd8
Thomas <tran.thomas.thien@gmail.com>
2024-01-10 08:43:50 +0100

    tout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-let menu = document.getElementById("menu");
 let btnRemonter = document.getElementById("btnRemonter");
 
 window.onscroll = function() {checkScroll()};
-
-window.onload = function() {
-    menu.style.display = "none";
-}
 
 function checkScroll(){
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
@@ -18,9 +13,4 @@ function checkScroll(){
 function remonter(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
-
-function menuShow(){
-    console.log(menu.style.display);
-    (menu.style.display == "none") ? menu.style.display = "block" : menu.style.display = "none"
 }
